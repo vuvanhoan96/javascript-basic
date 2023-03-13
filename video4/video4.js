509317f7c103ed68bc6b77a221b7164306d88750
vuvanhoan96 <vuhoan9603@gmail.com>
2023-03-13 22:46:29 +0700

    chapter 4

--- video4/video4.js
+++ video4/video4.js
@@ -6,3 +6,21 @@ let a ="Tôi là String - Chuỗi";
 let text = `I'm Hoan`
 console.log(a.trim().length);
 console.log(text);
+
+
+// Numbers
+
+let x = 5;
+let y = '5';
+
+console.log(x+y, x-y, x/y, x*y, x%y);
+
+// Ngoài phép cộng (+) thì các phép toán còn lại (-, *, /, %)
+// kiểu String y = '5' sẽ cố convert - chuyển thành Numbers rồi nó mới thực hiện.
+
+// Muốn chuyển từ String sang Number ta có 2 cách: 
+
+let z = Number(y);
+let o = +y;
+
+console.log(z, o);
