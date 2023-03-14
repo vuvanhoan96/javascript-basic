7c3a95ea7c560e8bee10ed91d753ad0a5b29f810
vuvanhoan96 <vuhoan9603@gmail.com>
2023-03-14 16:47:09 +0700

    bai video9, 10

---
+++ video9/video9.js
@@ -0,0 +1,20 @@
+console.log('Hello world from HTMl');
+
+let a = {
+    name: 'Hoan',
+    age: 25
+};
+// Arrays
+
+let b = ['Mu', 'Real', 'Baccar'];
+
+console.log('typeof b: ', typeof b, b);
+
+// Video10, == vs === , empty, null, undefined
+
+// === : type, value;
+// ==  : value;
+
+// Should use === 
+
+
